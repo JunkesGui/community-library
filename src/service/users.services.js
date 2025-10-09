36ce3e020230b8ff994e4a7b6063aaba91bbfe22
junkesgui <guilherme.junkes1@gmail.com>
2025-10-09 04:45:27 -0300

    Fix verifica se o usuario ja existe no DB a partir do email e retorna um erro para o cliente

--- src/service/users.services.js
+++ src/service/users.services.js
@@ -1,6 +1,8 @@
 import usersRepositories from '../repositories/users.respositories.js';
 
 async function createUserService(newUser){
+    const searchUser = await usersRepositories.findUserByEmail(newUser.email)
+    if (searchUser) throw new Error("User already exists!")
     const user = await usersRepositories.createUserRepository(newUser);
     return user;
 }
