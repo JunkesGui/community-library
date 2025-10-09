87993ad4306bd7678429facbad1d1d51389b4d62
junkesgui <guilherme.junkes1@gmail.com>
2025-10-09 04:44:26 -0300

    Fix adiciona o 'await' para a atribuicao do usuario (try)

--- src/controller/users.controllers.js
+++ src/controller/users.controllers.js
@@ -4,7 +4,7 @@ async function createUserController(req,res){
     const newUser = req.body;
 
     try {
-        const user = userServices.createUserService(newUser)
+        const user = await userServices.createUserService(newUser)
         res.status(201).send({user})
     } catch (err) {
         return res.status(400).send(err.message);        
