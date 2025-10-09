85746cbb5bcd44840f932b296f00ada4824bfacd
junkesgui <guilherme.junkes1@gmail.com>
2025-10-09 04:54:38 -0300

    Feat adiciona a encriptacao da senha do usuario no DB com a biblioteca bcrypt

--- src/service/users.services.js
+++ src/service/users.services.js
@@ -1,9 +1,13 @@
 import usersRepositories from '../repositories/users.respositories.js';
+import bcrypt from 'bcrypt';
 
 async function createUserService(newUser){
     const searchUser = await usersRepositories.findUserByEmail(newUser.email)
     if (searchUser) throw new Error("User already exists!")
-    const user = await usersRepositories.createUserRepository(newUser);
+
+    const passHash = await bcrypt.hash(newUser.password, 10)
+    const user = await usersRepositories.createUserRepository({...newUser, password: passHash});
+    if(!user) throw new Error("Error at the user creation")
     return user;
 }
 
