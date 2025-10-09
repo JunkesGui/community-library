990e9883f00283c3588a839d1378a5257366ce84
junkesgui <guilherme.junkes1@gmail.com>
2025-10-09 03:48:39 -0300

    Feat adiciona o arquivo de config da database e a criacao do userRepository

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('library_db.sqlite',  (err) =>{
+    if (err){
+        console.log('Failed to connect to DB: ', err.message);
+    } else{
+        console.log('Sucessfully connected to DB');
+    }
+})
+
+export default db;
