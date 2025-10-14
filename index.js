458a9c2f0a9cbfda460c25225724365b6d3c75d6
junkesgui <guilherme.junkes1@gmail.com>
2025-10-14 03:50:46 -0300

    fix: recebe a porta para rodar o servidor como variavel de ambiente (3000 se vazio)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import userRouters from './src/routes/users.routes.js'
+import "dotenv/config"
+
 const app = express();
-const port = 3000;
 app.use(express.json());
 
-app.use(userRouters)
+const port = process.env.PORT || 3000;
 
+app.use(userRouters)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
